4390119e31d8f7b401f3c477d144404083adebdb
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-20 16:19:32 -0400

    Update Disemvowel Trolls.cpp

--- Disemvowel Trolls.cpp
+++ Disemvowel Trolls.cpp
@@ -1,3 +1,26 @@
+/*                                                                                  Problem:
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+*/
+
+
+/*                                                                                  Solution:                                                                       */
+
+
+/* 
+    Simply checks if each character in the passed-in string is a vowel or not and if it isn't a vowel it is added to a different string which is returned at the end.
+    Not very proud of this solution, will come back and find a more graceful one.
+*/
+
 # include <string>
 
 std::string disemvowel (const std::string& str) {
