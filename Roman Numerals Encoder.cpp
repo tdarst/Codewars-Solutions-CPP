78d08252b9bb0e900fec707f94d64a82d74db208
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-20 15:19:32 -0400

    Update Roman Numerals Encoder.cpp

--- Roman Numerals Encoder.cpp
+++ Roman Numerals Encoder.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+/*
+                                                                                Problem:
+
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman
+numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 
+1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in 
+descending order.
+
+Example:
+
+solution("XXI"); // => 21
+
+*/
+
+
 int solution(string roman) {
   /* integer to accumulate value of roman numerals */
   int total = 0;
