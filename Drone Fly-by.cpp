4374d0b446a55cd65bcd3457800c538a47c36f04
tdarst <51347370+tdarst@users.noreply.github.com>
2022-09-20 15:21:44 -0400

    Add files via upload

---
+++ Drone Fly-by.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+/*
+
+The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+
+In this kata we will recreate that stunt... sort of.
+
+You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+
+The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+
+Return the resulting lamps string. 
+
+*/
+
+
+std::string flyBy(std::string lamp, std::string drone){
+  int n = drone.length();
+  
+  for (int i = 0; i < n; i++)
+  {
+    lamp[i] = 'o';
+  }
+  
+  return lamp;
+}
