0592304827e05d52ca63078b64b51f093f460dad
tdarst <51347370+tdarst@users.noreply.github.com>
2022-10-13 14:26:03 -0400

    Update Interlocking Binary Pairs.cpp

--- Interlocking Binary Pairs.cpp
+++ Interlocking Binary Pairs.cpp
@@ -1,3 +1,15 @@
+/*                                                                                Problem:
+Write a function that checks if two non-negative integers make an "interlocking binary pair".
+numbers can be interlocked if their binary representations have no 1's in the same place
+comparisons are made by bit position, starting from right to left (see the examples below)
+when representations are of different lengths, the unmatched left-most bits are ignored.
+*/
+
+/*                                                                                Solution: 
+Take given numbers and put them through binConvert which returns a vector representation of their binary equivalents but returns the equivalent backwards so that
+length difference is not an issue. Compares the two vectors element to element to see if they both have a one at the same index.
+*/
+
 std::vector<int> binConvert(int c){
   std::vector<int> bin;
   
